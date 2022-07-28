d9b15dddc9e623a9d6e2fbe4bec286c8fc583d77
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2022-07-28 20:46:00 +0300

    Add unit and integration tests

---
+++ src/main/kotlin/com/example/C.kt
@@ -0,0 +1,11 @@
+package com.example
+
+object C {
+    fun f(): Int {
+        return 42
+    }
+
+    fun g(): Int {
+        return 42
+    }
+}
---
+++ src/test/kotlin/com/example/CTest.kt
@@ -0,0 +1,14 @@
+package com.example
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class CTest {
+    @Test
+    fun t() {
+        assertEquals(42, C.f())
+    }
+}
